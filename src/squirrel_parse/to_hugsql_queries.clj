4193d5700bd51112e0a761be21f841be9c0a8c15
Simon Brooke <simon@journeyman.cc>
2018-03-17 15:30:46 +0000

    Now generating credible-seeming (but untested) routes file

--- src/squirrel_parse/to_hugsql_queries.clj
+++ src/squirrel_parse/to_hugsql_queries.clj
@@ -39,10 +39,10 @@
 (defn order-by-clause [entity-map]
   (let
     [entity-name (:name (:attrs entity-map))
-        preferred (map
-                  #(:name (:attrs %))
-                  (filter #(= (-> % :attrs :distinct) "user")
-                          (-> entity-map :content :properties vals)))]
+     preferred (map
+                 #(:name (:attrs %))
+                 (filter #(= (-> % :attrs :distinct) "user")
+                         (-> entity-map :content :properties vals)))]
     (str
       "ORDER BY " entity-name "."
       (s/join
@@ -52,20 +52,28 @@
 
 (defn insert-query [entity-map]
   (let [entity-name (:name (:attrs entity-map))
-        pretty-name (s/replace (s/replace entity-name #"_" "-") #"s$" "")
+        pretty-name (singularise entity-name)
         all-property-names (map #(:name (:attrs %)) (vals (:properties (:content entity-map))))
-        ]
-    (str "-- :name create-" pretty-name "! :<!\n"
-         "-- :doc creates a new " pretty-name " record\n"
-         "INSERT INTO " entity-name " ("
-         (s/join ",\n\t" all-property-names)
-         ")\nVALUES ("
-         (s/join ",\n\t" (map keyword all-property-names))
-         ")"
-         (if
-           (has-primary-key? entity-map)
-           (str "\nreturning " (s/join ",\n\t" (key-names entity-map))))
-         "\n\n")))
+        query-name (str "create-" pretty-name "!")
+        signature " :! :n"]
+    (hash-map
+      (keyword query-name)
+      {:name query-name
+       :signature signature
+       :entity entity-map
+       :type :insert-1
+       :query
+       (str "-- :name " query-name " " signature "\n"
+            "-- :doc creates a new " pretty-name " record\n"
+            "INSERT INTO " entity-name " ("
+            (s/join ",\n\t" all-property-names)
+            ")\nVALUES ("
+            (s/join ",\n\t" (map keyword all-property-names))
+            ")"
+            (if
+              (has-primary-key? entity-map)
+              (str "\nreturning " (s/join ",\n\t" (key-names entity-map))))
+            "\n\n")})))
 
 
 (defn update-query [entity-map]
@@ -74,64 +82,109 @@
       (has-primary-key? entity-map)
       (has-non-key-properties? entity-map))
     (let [entity-name (:name (:attrs entity-map))
-          pretty-name (s/replace (s/replace entity-name #"_" "-") #"s$" "")
+          pretty-name (singularise entity-name)
           property-names (remove
                            nil?
                            (map
                              #(if (= (:tag %) :property) (:name (:attrs %)))
-                             (vals (:properties (:content entity-map)))))]
-      (str "-- :name update-" pretty-name "! :! :n\n"
-           "-- :doc updates an existing " pretty-name " record\n"
-           "UPDATE " entity-name "\n"
-           "SET "
-           (s/join ",\n\t" (map #(str % " = " (keyword %)) property-names))
-           "\n"
-           (where-clause entity-map)
-           "\n\n"))))
+                             (vals (:properties (:content entity-map)))))
+          query-name (str "update-" pretty-name "!")
+          signature ":! :n"]
+      (hash-map
+        (keyword query-name)
+        {:name query-name
+         :signature signature
+         :entity entity-map
+         :type :update-1
+         :query
+         (str "-- :name " query-name " " signature "\n"
+              "-- :doc updates an existing " pretty-name " record\n"
+              "UPDATE " entity-name "\n"
+              "SET "
+              (s/join ",\n\t" (map #(str % " = " (keyword %)) property-names))
+              "\n"
+              (where-clause entity-map)
+              "\n\n")}))
+    {}))
 
 
 (defn select-query [entity-map]
   (if
     (has-primary-key? entity-map)
     (let [entity-name (:name (:attrs entity-map))
-          pretty-name (s/replace (s/replace entity-name #"_" "-") #"s$" "")]
-      (str "-- :name get-" pretty-name " :? :1\n"
-           "-- :doc selects an existing " pretty-name " record\n"
-           "SELECT * FROM " entity-name "\n"
-           (where-clause entity-map)
-           "\n"
-           (order-by-clause entity-map)
-           "\n\n"))))
+          pretty-name (singularise entity-name)
+          query-name (str "get-" pretty-name)
+          signature ":? :1"]
+      (hash-map
+        (keyword query-name)
+        {:name query-name
+         :signature signature
+         :entity entity-map
+         :type :select-1
+         :query
+         (str "-- :name " query-name " " signature "\n"
+              "-- :doc selects an existing " pretty-name " record\n"
+              "SELECT * FROM " entity-name "\n"
+              (where-clause entity-map)
+              "\n"
+              (order-by-clause entity-map)
+              "\n\n")}))
+    {}))
 
 
-(defn list-query [entity-map]
+(defn list-query
+  "Generate a query to list records in the table represented by this `entity-map`.
+  Parameters `:limit` and `:offset` may be supplied. If not present limit defaults
+  to 100 and offset to 0."
+  [entity-map]
   (let [entity-name (:name (:attrs entity-map))
-        pretty-name (s/replace (s/replace entity-name #"_" "-") #"s$" "")]
-    (str "-- :name list-" pretty-name " :? :*\n"
-         "-- :doc lists all existing " pretty-name " records\n"
-         "SELECT * FROM " entity-name "\n"
-         (order-by-clause entity-map)
-         "\n\n")))
+        pretty-name (singularise entity-name)
+        query-name (str "list-" entity-name)
+        signature ":? :*"]
+    (hash-map
+      (keyword query-name)
+      {:name query-name
+       :signature signature
+       :entity entity-map
+       :type :select-many
+       :query
+       (str "-- :name " query-name " " signature "\n"
+            "-- :doc lists all existing " pretty-name " records\n"
+            "SELECT * FROM " entity-name "\n"
+            (order-by-clause entity-map) "\n"
+            "--~ (if (:offset params) \"OFFSET :offset \") \n"
+            "--~ (if (:limit params) \"LIMIT :limit\" \"LIMIT 100\")"
+            "\n\n")})))
 
 
 (defn foreign-queries [entity-map entities-map]
   (let [entity-name (:name (:attrs entity-map))
-        pretty-name (s/replace (s/replace entity-name #"_" "-") #"s$" "")
+        pretty-name (singularise entity-name)
         links (filter #(-> % :attrs :entity) (-> entity-map :content :properties vals))]
     (apply
-      str
+      merge
       (map
         #(let [far-name (-> % :attrs :entity)
                far-entity ((keyword far-name) entities-map)
                pretty-far (s/replace (s/replace far-name #"_" "-") #"s$" "")
                farkey (-> % :attrs :farkey)
-               link-field (-> % :attrs :name)]
-           (str "-- :name list-" entity-name "-by-" pretty-far " :? :*\n"
-                "-- :doc lists all existing " pretty-name " records related to a given " pretty-far "\n"
-                "SELECT * \nFROM " entity-name "\n"
-                "WHERE " entity-name "." link-field " = :id\n"
-                (order-by-clause entity-map)
-                "\n\n"))
+               link-field (-> % :attrs :name)
+               query-name (str "list-" entity-name "-by-" pretty-far)
+               signature ":? :*"]
+           (hash-map
+             (keyword query-name)
+             {:name query-name
+              :signature signature
+              :entity entity-map
+              :type :select-one-to-many
+              :far-entity far-entity
+              :query
+              (str "-- :name " query-name " " signature "\n"
+                   "-- :doc lists all existing " pretty-name " records related to a given " pretty-far "\n"
+                   "SELECT * \nFROM " entity-name "\n"
+                   "WHERE " entity-name "." link-field " = :id\n"
+                   (order-by-clause entity-map)
+                   "\n\n")}))
         links))))
 
 
@@ -145,16 +198,26 @@
         near-name (-> near :attrs :name)
         link-name (-> link :attrs :name)
         far-name (-> far :attrs :name)
-        pretty-far (singularise far-name)]
-    (println links)
-    (str "-- :name list-" link-name "-" near-name "-by-" pretty-far " :? :*\n"
-         "-- :doc lists all existing " near-name " records related through " link-name " to a given " pretty-far "\n"
-         "SELECT "near-name ".*\n"
-         "FROM " near-name ", " link-name "\n"
-         "WHERE " near-name "." (first (key-names near)) " = " link-name "." (-> (links (keyword near-name)) :attrs :name) "\n\t"
-         "AND " link-name "." (-> (links (keyword far-name)) :attrs :name) " = :id\n"
-         (order-by-clause near)
-         "\n\n")))
+        pretty-far (singularise far-name)
+        query-name (str "list-" link-name "-" near-name "-by-" pretty-far)
+        signature ":? :*"]
+    (hash-map
+      (keyword query-name)
+      {:name query-name
+       :signature signature
+       :entity link
+       :type :select-many-to-many
+       :near-entity near
+       :far-entity far
+       :query
+       (str "-- :name " query-name " " signature " \n"
+            "-- :doc lists all existing " near-name " records related through " link-name " to a given " pretty-far "\n"
+            "SELECT "near-name ".*\n"
+            "FROM " near-name ", " link-name "\n"
+            "WHERE " near-name "." (first (key-names near)) " = " link-name "." (-> (links (keyword near-name)) :attrs :name) "\n\t"
+            "AND " link-name "." (-> (links (keyword far-name)) :attrs :name) " = :id\n"
+            (order-by-clause near)
+            "\n\n")})))
 
 
 (defn link-table-queries [entity-map entities-map]
@@ -164,9 +227,9 @@
                 (remove nil? (map #(-> % :attrs :entity) (-> entity-map :content :properties vals))))
      pairs (combinations entities 2)]
     (apply
-      str
+      merge
       (map
-        #(str
+        #(merge
            (link-table-query (nth % 0) entity-map (nth % 1))
            (link-table-query (nth % 1) entity-map (nth % 0)))
         pairs))))
@@ -177,24 +240,34 @@
   (if
     (has-primary-key? entity-map)
     (let [entity-name (:name (:attrs entity-map))
-          pretty-name (s/replace (s/replace entity-name #"_" "-") #"s$" "")]
-      (str "-- :name delete-" pretty-name "! :! :n\n"
-           "-- :doc updates an existing " pretty-name " record\n"
-           "DELETE FROM " entity-name "\n"
-           (where-clause entity-map)
-           "\n\n"))))
+          pretty-name (singularise entity-name)
+          query-name (str "delete-" pretty-name "!")
+          signature ":! :n"]
+      (hash-map
+        (keyword query-name)
+        {:name query-name
+         :signature signature
+         :entity entity-map
+         :type :delete-1
+         :query
+         (str "-- :name " query-name ":! " signature "\n"
+              "-- :doc updates an existing " pretty-name " record\n"
+              "DELETE FROM " entity-name "\n"
+              (where-clause entity-map)
+              "\n\n")}))))
 
 
 (defn queries
   [entity-map entities-map]
-  (str
+  (merge
+    {}
     (insert-query entity-map)
     (update-query entity-map)
     (delete-query entity-map)
     (if
       (is-link-table? entity-map)
       (link-table-queries entity-map entities-map)
-      (str
+      (merge
         (select-query entity-map)
         (list-query entity-map)
         (foreign-queries entity-map entities-map)))))
@@ -206,6 +279,15 @@
   ([migrations-path output]
    (let
      [adl-struct (migrations-to-xml migrations-path "Ignored")
-      file-content (apply str (map #(queries % adl-struct) (vals adl-struct)))]
+      file-content (apply
+                     str
+                     (doall (map
+                              #(:query %)
+                              (vals
+                                (apply
+                                  merge
+                                  (map
+                                    #(queries % adl-struct)
+                                    (vals adl-struct)))))))]
      (spit output file-content)
      file-content)))
